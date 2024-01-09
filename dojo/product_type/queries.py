6ef371410d5f2f5e23f9c4e2befd202712014a43
Inês Martins <i.martins@swordhealth.com>
2024-01-09 16:06:59 +0000

    removes unused imports

--- dojo/product_type/queries.py
+++ dojo/product_type/queries.py
@@ -1,7 +1,6 @@
 from crum import get_current_user
-from django.db.models import Exists, OuterRef, Q
 from dojo.models import Product_Type, Product_Type_Member, Product_Type_Group
-from dojo.authorization.authorization import get_roles_for_permission, user_has_global_permission, user_has_permission, \
+from dojo.authorization.authorization import user_has_global_permission, user_has_permission, \
     role_has_permission
 from dojo.group.queries import get_authorized_groups
 from dojo.authorization.roles_permissions import Permissions
